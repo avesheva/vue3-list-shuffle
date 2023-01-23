5234dfc2159071430121c9a6b946887710c27439
avesheva <shevochkandrey@gmail.com>
2023-01-23 20:06:52 +0200

    Changed shuffle function logic for different height items.

--- lib/ListShuffle.vue
+++ lib/ListShuffle.vue
@@ -8,7 +8,7 @@ export interface IProps {
   restoreOrder?: number | string | boolean,
   shuffle?: number | string | boolean,
 }
-export type ListItemDataType = { index: number, top: number }
+export type ListItemDataType = { index: number, top: number, bottom: number, height: number }
 
 const props = withDefaults(defineProps<IProps>(), {
   id: 'listWrapper',
@@ -36,11 +36,25 @@ const shuffleList = (toInitOrder = false) => {
   if (!listWrapper) return
 
   const newOrder = toInitOrder ? [ ...initialOrder ] : shuffleArray(initialOrder)
+  const updatedCoordinates: ListItemDataType[] = []
 
-  for (let i = 0; i < listWrapper.children.length; i++) {
-    const currentElement = listWrapper.children[i] as HTMLElement
-    const newIndex = newOrder.findIndex((item  ) => item.index === i)
-    const top = initialOrder[newIndex].top - initialOrder[i].top
+  for (let i = 0; i < newOrder.length; i++) {
+    updatedCoordinates[i] = { ...newOrder[i] }
+
+    if (i === 0) {
+      updatedCoordinates[i].top = initialOrder[i].top
+      updatedCoordinates[i].bottom = updatedCoordinates[i].top + updatedCoordinates[i].height
+      continue
+    }
+
+    updatedCoordinates[i].top = updatedCoordinates[i - 1].bottom
+    updatedCoordinates[i].bottom = updatedCoordinates[i].top + updatedCoordinates[i].height
+  }
+
+  for (let i = 0; i < updatedCoordinates.length; i++) {
+    const item = updatedCoordinates[i]
+    const currentElement = listWrapper.children[item.index] as HTMLElement
+    const top = item.top - initialOrder[item.index].top
 
     currentElement.style.transform = `translate(0, ${ top }px)`
   }
@@ -60,11 +74,12 @@ onMounted(() => {
   if (listWrapper) {
     for (let i = 0; i < listWrapper.children.length; i++) {
       const element = listWrapper.children[i] as HTMLElement
+      const clientRect = element.getBoundingClientRect()
 
       element.style.transitionProperty = 'transform'
       element.style.transitionDuration = `${ props.duration }s`
 
-      initialOrder.push({ index: i, top: element.getBoundingClientRect().top })
+      initialOrder.push({ index: i, top: clientRect.top, bottom: clientRect.bottom, height: clientRect.height })
     }
   }
 
